02eb148b0e3985a6aff23405e671f60861ad8a8b
Keiichi Lindley <digizn@gmail.com>
2016-01-06 07:24:25 -0700

    Change orders to inbox in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var walker = require('./walker'),
     queue  = require('./queue'),
     parser = require('./parser'),
-    jobs = new queue({concurrent: 1}),
-    orders = new walker({
+      jobs = new queue({concurrent: 1}),
+     inbox = new walker({
       dir: __dirname + '/inbox'
     , max: 10
     });
 
-orders.on('file', function(file){
+inbox.on('file', function(file){
   jobs.add(file, function(item){
     var parse = new parser(item)
     parse.on('end', function(order){
