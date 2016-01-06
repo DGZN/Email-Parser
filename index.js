2f2ef4035eb90349ed8ca728dc041e3eb0636eeb
Keiichi Lindley <digizn@gmail.com>
2016-01-05 22:39:22 -0700

    :)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var walker = require('./walker'),
-    queue  = require('./queue')
-    parser = new queue({max: 1});
-   orders  = new walker(__dirname+'/inbox'),
+    queue  = require('./queue'),
+    parser = new queue({max: 1}),
+    orders = new walker(__dirname+'/inbox');
 
 orders.on('file', function(file){
   parser.add(file)
