e6e3b1b6ff1f4c06ce714a07a3771fac451b8eea
Keiichi Lindley <digizn@gmail.com>
2016-01-06 23:10:27 -0700

    Implement solid parser pattern

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var walker = require('./lib/walker'),
     queue  = require('./lib/queue'),
-    debug  = require('debug')('app')
+    orders = require('debug')('orders')
     parser = require('./lib/parser');
 
 var jobs  = new queue({concurrent: 1});
-var inbox = new walker({dir: __dirname + '/inbox', max: 100});
+var inbox = new walker({dir: __dirname + '/inbox', max: 10000});
 
 inbox.on('file', function(file){
   jobs.add(file, function(file, next){
-    next()
-    // var parse = new parser(item)
-    // parse.on('end', function(order){
-    //   console.log(order);
-    //   jobs.next()
-    // })
+    var parse = new parser(file)
+    parse.on('end', function(order){
+      if (order)
+        orders(order)
+      next()
+    })
+  })
+}).on('end', function(files){
+  jobs.finish(() => {
+    orders('Parsed ' + files.count + ' files')
   })
 })
