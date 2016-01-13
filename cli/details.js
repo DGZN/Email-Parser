9afb15851e378db6c4cc7d8937c7f0b9249d06b4
Keiichi Lindley <digizn@gmail.com>
2016-01-13 15:08:12 -0700

    Begin implement product details parser

--- cli/details.js
+++ cli/details.js
@@ -7,33 +7,63 @@ const cheerio   = require("cheerio");
 const Lazy      = require('lazy');
 const Products  = require('../models/orderproducts');
 
-const LIMIT = 1;
+const LIMIT = 20;
 
-var Items  = new Lazy;
+var Options  = new Lazy;
 var Extras = new Lazy;
 
-Items.forEach((product) => {
-  var types = [];
-  var values = product.item.split(':');
-  if (values.length==1) {
-    debug(values)
+var products = [];
+
+Options.forEach((product) => {
+  var values = product.option.split(':');
+  if (values.length==2) {
+    addOption(product.name, values)
   }
+}).on('pipe', () => {
+  debug('Finished processing all files')
+  console.log(require('util').inspect(products, { depth: null }));
 })
 
 Extras.forEach((product) => {
-  debug('# '+product.name)
-  product.details.map((item) => {
-    Items.emit('data', {
-      item: item[0]
+  product.details.map((option) => {
+    Options.emit('data', {
+      option: option[0]
     , name: product.name
     })
   })
+}).on('pipe', () => {
+  Options.emit('end')
 })
 
 Products.find(function(err, products) {
   if (err)
-    throw err;
+    return error(err)
   products.map((product) => {
-    Extras.emit('data', product)
+    addProduct(product)
   })
+  Extras.emit('end');
 }).limit(LIMIT);
+
+function addProduct(product){
+  if (Object.keys(products).indexOf(product.name)==-1) {
+    products.push(product.name);
+    products[product.name] = {
+      options: {}
+    }
+  }
+  Extras.emit('data', product)
+}
+
+function addOption(name, values){
+  var key   = values[0].trim()
+  var value = values[1].trim()
+  if (typeof products[name].options == 'object') {
+    if (Object.keys(products[name]['options']).indexOf(key)==-1) {
+      products[name]['options'][key] = [];
+      products[name]['options'][key].push(value)
+    } else {
+      if (products[name]['options'][key].indexOf(value)==-1)
+        products[name]['options'][key].push(value)
+    }
+  }
+}
